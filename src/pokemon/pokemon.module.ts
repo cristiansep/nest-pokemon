0a3be22107191f01e24a483e036f2bcf76c8b186
cristiansep <cristiansepulvedamendez@gmail.com>
2024-05-13 10:14:47 -0400

    First commit

---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PockemonSchema, Pokemon } from './entities/pokemon.entity';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PockemonSchema
+      }
+    ])
+  ],
+  exports: [MongooseModule]
+})
+export class PokemonModule {}
